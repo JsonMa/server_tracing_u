ff8571d355033bc5640626fe08d9693e8c082661
JsonMa <mahao@iot.chinamobile.com>
2019-04-28 11:07:45 +0800

    fix(config): update db config

--- config/config.default.js
+++ config/config.default.js
@@ -42,21 +42,19 @@ module.exports = () => {
     codeUrl: 'https://api.weixin.qq.com/wxa/getwxacodeunlimit'
   };
 
+  exports.mongoose = {
+    client: {
+      url: 'mongodb://tracing_u:tracing_u123456@127.0.0.1/tracing_u',
+      options: {}
+    }
+  };
+
   exports.redis = {
     client: {
       port: 6379,
       host: 'localhost',
       password: 'tracing_redis123456',
       db: 0
-    }
-  };
-
-  exports.mongoose = {
-    client: {
-      url: 'mongodb://127.0.0.1/tracing_u',
-      options: {
-        useNewUrlParser: true
-      }
     }
   };
 
