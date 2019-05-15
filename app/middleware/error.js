34b09b15d1a327dc6925fbc4378fb1e99782a608
JsonMa <mahao@iot.chinamobile.com>
2019-05-15 09:43:39 +0800

    fix: fix errors response

--- app/middleware/error.js
+++ app/middleware/error.js
@@ -9,22 +9,18 @@ module.exports = () =>
     try {
       yield next;
     } catch (e) {
-      /* istanbul ignore next */
-      if ((this.app.isProd || this.app.isTest) && !this.is('json')) throw e;
-      /* istanbul ignore next */
-      this.app.logger.error('');
       if (e.name === 'AJV_ERROR') {
         this.body = {
           code: 400,
           msg: e.jse_shortmsg,
-          errors: this.app.isProd ? undefined : e.jse_info,
+          errors: e.jse_info,
         };
         this.status = 400;
       } else if (e instanceof VError) {
         this.body = {
           code: e.code,
           msg: e.message,
-          errors: this.app.isProd ? undefined : e.stack,
+          errors: e.stack,
         };
         this.status = e.status || 200;
       } else {
@@ -32,7 +28,7 @@ module.exports = () =>
         this.body = {
           code: 10001,
           msg: e.message || '服务器内部错误',
-          errors: this.app.isProd ? undefined : e.stack,
+          errors: e.stack,
         };
         this.status = e.status || 500;
       }
