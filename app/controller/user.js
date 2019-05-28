25f4239cc691807c7f7cec040e7b6735ed825fe7
JsonMa <mahao@iot.chinamobile.com>
2019-05-28 15:03:09 +0800

    fix: resolve reg bugs

--- app/controller/user.js
+++ app/controller/user.js
@@ -231,7 +231,7 @@ module.exports = app => {
         token,
       } = ctx.loginPermission();
       await ctx.verify(createRule, ctx.request.body);
-      const isUserExist = await ctx.service.user.find(openid);
+      const isUserExist = await ctx.service.user.findOne(openid);
       ctx.error(isUserExist.role_type === 'unauthed', 10004, '注册失败，该用户已注册');
       const {
         role_type,
